b1fab77322e628db42791400b1f4447d40752718
ndang <ndang2319@gmail.com>
2025-06-13 17:14:26 +0000

    feat: integrate Redux for state management

--- apps/web/components/providers/index.tsx
+++ apps/web/components/providers/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type * as React from "react";
+import StoreProvider from "./store-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 			disableTransitionOnChange
 			enableColorScheme
 		>
-			{children}
+			<StoreProvider>{children}</StoreProvider>
 		</NextThemesProvider>
 	);
 }
