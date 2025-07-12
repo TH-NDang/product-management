36a6f7e54deae7ebbb5a95b5076f28b7278d69e6
ndang <ndang2319@gmail.com>
2025-07-12 10:07:12 +0700

    temp: some change in api folder

--- apps/api/core/database.py
+++ apps/api/core/database.py
@@ -2,7 +2,6 @@ from fastapi import Depends
 from typing import Annotated
 from sqlmodel import SQLModel, Session, create_engine
 
-from core import database
 from core.config import get_settings
 
 # Why use it: https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata-order-matters
@@ -16,12 +15,12 @@ engine = create_engine(database_url, echo=True)
 
 
 def get_session():
-    with Session(database.engine) as session:
+    with Session(engine) as session:
         yield session
 
 
 def create_db_and_tables():
-    SQLModel.metadata.create_all(database.engine)
+    SQLModel.metadata.create_all(engine)
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
